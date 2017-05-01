73b7274b043d36664f9457db062d1bd2fe704bb7
Ethan Rivas <ethan-rivas@hotmail.com>
2017-04-30 21:07:32 -0500

    comment model with associations
    comment, post and user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :posts
+  has_many :comments
+
   accepts_nested_attributes_for :posts, allow_destroy: true
 end
